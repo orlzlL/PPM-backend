53f89fb733e7861b643c98f4c1acfff8f97b768a
장민석 <64572078+ms1011@users.noreply.github.com>
2024-05-10 16:07:39 +0900

    Revert "[PR] 대시보드 그래프 데이터 저장을 위한 mongoDB 도입"

--- src/main/java/org/omoknoone/ppm/PpmApplication.java
+++ src/main/java/org/omoknoone/ppm/PpmApplication.java
@@ -5,10 +5,8 @@ import org.modelmapper.config.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
 public class PpmApplication {
 
     public static void main(String[] args) {
